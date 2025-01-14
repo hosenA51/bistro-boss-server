2e2809023788ae155ab4989c6b993737fd87e1d4
hosenA51 <hosenahmed2552@gmail.com>
2025-01-14 12:56:06 +0600

    implement stats functionality

--- index.js
+++ index.js
@@ -14,6 +14,7 @@ app.use(express.json());
 
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.nweo5.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+// const uri = "mongodb://localhost:27017";
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -59,13 +60,13 @@ async function run() {
         }
 
         // use verify admin after verifyToken
-        const verifyAdmin = async(req, res, next) => {
+        const verifyAdmin = async (req, res, next) => {
             const email = req.decoded.email;
-            const query = {email: email};
+            const query = { email: email };
             const user = await userCollection.findOne(query);
             const isAdmin = user?.role === 'admin';
-            if(!isAdmin){
-                return res.status(403).send({message: 'forbidden access'});
+            if (!isAdmin) {
+                return res.status(403).send({ message: 'forbidden access' });
             }
             next();
         }
@@ -130,23 +131,23 @@ async function run() {
             res.send(result);
         })
 
-        app.get('/menu/:id', async(req, res)=>{
+        app.get('/menu/:id', async (req, res) => {
             const id = req.params.id;
-            const query = {_id: new ObjectId(id)}
+            const query = { _id: new ObjectId(id) }
             const result = await menuCollection.findOne(query);
             res.send(result);
         })
 
-        app.post('/menu', verifyToken, verifyAdmin, async(req, res)=>{
+        app.post('/menu', verifyToken, verifyAdmin, async (req, res) => {
             const item = req.body;
             const result = await menuCollection.insertOne(item);
             res.send(result);
         })
 
-        app.patch('/menu/:id', async(req, res) =>{
+        app.patch('/menu/:id', async (req, res) => {
             const item = req.body;
             const id = req.params.id;
-            const filter = {_id: new ObjectId(id)}
+            const filter = { _id: new ObjectId(id) }
             const updatedDoc = {
                 $set: {
                     name: item.name,
@@ -161,9 +162,9 @@ async function run() {
             res.send(result);
         })
 
-        app.delete('/menu/:id', verifyToken, verifyAdmin, async (req, res)=>{
+        app.delete('/menu/:id', verifyToken, verifyAdmin, async (req, res) => {
             const id = req.params.id;
-            const query = {_id: new ObjectId(id)}
+            const query = { _id: new ObjectId(id) }
             const result = await menuCollection.deleteOne(query);
             res.send(result);
         })
@@ -193,8 +194,8 @@ async function run() {
         })
 
         // payment intent
-        app.post('/create-payment-intent', async(req, res)=>{
-            const {price} = req.body;
+        app.post('/create-payment-intent', async (req, res) => {
+            const { price } = req.body;
             const amount = parseInt(price * 100);
 
             const paymentIntent = await stripe.paymentIntents.create({
@@ -208,27 +209,109 @@ async function run() {
             })
         });
 
-        app.get('/payments/:email', verifyToken, async(req, res)=>{
-            const query = {email: req.params.email}
-            if(req.params.email !== req.decoded.email){
-                return res.status(403).send({message: 'forbidden access'});
+        app.get('/payments/:email', verifyToken, async (req, res) => {
+            const query = { email: req.params.email }
+            if (req.params.email !== req.decoded.email) {
+                return res.status(403).send({ message: 'forbidden access' });
             }
             const result = await paymentCollection.find(query).toArray();
             res.send(result);
         })
 
-        app.post('/payments', async(req, res)=>{
+        app.post('/payments', async (req, res) => {
             const payment = req.body;
             const paymentResult = await paymentCollection.insertOne(payment);
 
             // carefully delete each item from the cart
             console.log('payment info', payment);
-            const query = {_id: {
-                $in: payment.cartIds.map(id => new ObjectId(id))
-            }};
+            const query = {
+                _id: {
+                    $in: payment.cartIds.map(id => new ObjectId(id))
+                }
+            };
             const deleteResult = await cartCollection.deleteMany(query);
 
-            res.send({paymentResult, deleteResult});
+            res.send({ paymentResult, deleteResult });
+        })
+
+        // stats or analytics
+        app.get('/admin-stats', verifyToken, verifyAdmin, async (req, res) => {
+            const users = await userCollection.estimatedDocumentCount();
+            const menuItems = await menuCollection.estimatedDocumentCount();
+            const orders = await paymentCollection.estimatedDocumentCount();
+
+            // this is not the best way
+            // const payments = await paymentCollection.find().toArray();
+            // const revenue = payments.reduce( (total, payment) => total + payment.price, 0);
+
+            const result = await paymentCollection.aggregate([
+                {
+                    $group: {
+                        _id: null,
+                        totalRevenue: {
+                            $sum: '$price'
+                        }
+                    }
+                }
+            ]).toArray();
+
+            const revenue = result.length > 0 ? result[0].totalRevenue : 0;
+
+            res.send({
+                users,
+                menuItems,
+                orders,
+                revenue
+            })
+        })
+
+        // order status
+        /**
+         * -----------------------------
+         *      NON-Efficient way
+         * -----------------------------
+         * 1. load all the payments
+         * 2. for every menuItemIds (which is an array), go find the item from menu collection.
+         * 3. for every item in the menu collection that you found from a payment entry (document)
+         */
+
+        // using aggregate pipeline
+        app.get('/order-stats', verifyToken, verifyAdmin, async (req, res) => {
+            const result = await paymentCollection.aggregate([
+                {
+                    $unwind: '$menuItemIds'
+                },
+                {
+                    $lookup: {
+                        from: 'menu',
+                        localField: 'menuItemIds',
+                        foreignField: '_id',
+                        as: 'menuItems'
+                    }
+                },
+                {
+                    $unwind: '$menuItems'
+                },
+                {
+                    $group: {
+                        _id: '$menuItems.category',
+                        quantity: {
+                            $sum: 1
+                        },
+                        revenue: {$sum: '$menuItems.price'}
+                    }
+                },
+                {
+                    $project: {
+                        _id: 0,
+                        category: '$_id',
+                        quantity: '$quantity',
+                        revenue: '$revenue'
+                    }
+                }
+            ]).toArray();
+
+            res.send(result);
         })
 
         // Send a ping to confirm a successful connection
